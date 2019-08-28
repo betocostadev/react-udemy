90ac7cc1c1f49240c963aaad09a2218537d0f4ad
betocostadev <robertomarx@gmail.com>
2019-08-28 19:57:05 -0300

    started card component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,42 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello, Beto!'
-    }
+      /* Adding unique ids to help React for changes. Using the ids, react can change only
+      one element if needed, and not all of them */
+      monsters: [
+      ]
+    };
+  }
+
+  // Life Cycle Methods => componentDidMount
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then(response => response.json())
+  //   .then(users => this.setState({ monsters: users }))
+  // }
+  // Making it async
+  async componentDidMount() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const json = await response.json();
+    this.setState({ monsters: json })
   }
 
   render() {
     return (
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <input
-            name="input"
-            placeholder="good">
-          </input>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={() => this.setState({ string: 'Hello, Cornholio!' })}>
-            Change text
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-          </a>
-        </header>
+      <div className="App">
+        {/* <header className="App-header">
+          Monsters Rolodex
+        </header> */}
+        <CardList>
+          { this.state.monsters.map(monster => (
+              <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+        </CardList>
+
+
       </div>
     );
   }
